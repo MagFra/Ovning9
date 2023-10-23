02a9e75ba4a9d65c7a4c24a37ed3d74dec4ca083
MagFra <47413425+magfra@users.noreply.github.com>
2023-10-23 17:39:19 +0200

    Öning 9 - Bish Bosh 2.0 Inlämling!

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,46 @@
+const form = document.querySelector('#inputForm');
+const formBtn = document.querySelector('#formBtn');
 const output = document.querySelector('#output');
-let inputBish = 3;
-let inputBosh = 4;
-let inputEnd = 100;
 
 
-const biscBosh = BishBosh(inputBish, inputBosh, inputEnd)
-for (let value of biscBosh) {
-    console.log(value);
-    output.innerHTML += value + ', ';
+let inputBish;
+let inputBosh;
+let inputEnd;
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    start();
+
+    e.target.reset();
+})
+
+
+function start() {
+
+    inputBish = document.querySelector('#bish').value;
+    inputBosh = document.querySelector('#bosh').value;
+    inputEnd = document.querySelector('#max').value;
+
+    output.innerHTML = `Bish: ${inputBish}, Bosh: ${inputBosh}, Max: ${inputEnd}<br>`;
+    output.innerHTML += `Tänker...<br>`;
+
+    const biscBosh = BishBosh(inputBish, inputBosh, inputEnd)
+    setTimeout(() => {
+        for (let value of biscBosh) {
+            console.log(value);
+            print(value);
+        }
+    }, 4750)
 }
 
+function print(value) {
+    setTimeout(() => {
+        output.innerHTML += value + ', ';
+    }, 250)
+}
 
 
 function BishBosh(bish, bosh, end) {
